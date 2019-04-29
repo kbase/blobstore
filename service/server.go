b3896f405b868c546cba08a0fb9bb2bcf4cd98a3
Gavin <gaprice@lbl.gov>
2019-04-29 15:00:57 -0700

    Basic server working

--- service/server.go
+++ service/server.go
@@ -1,2 +1,31 @@
 package service
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Server the blobstore server
+type Server struct {
+	mux *mux.Router
+}
+
+// New create a new server.
+func New() (*Server) {
+	router := mux.NewRouter()
+	// r.Use(loggingMiddleWare)
+	router.HandleFunc("/", rootHandler)
+	s := &Server{mux: router}
+	return s
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO return info about server
+	fmt.Fprintf(w, "Hello there!\n")
+}
