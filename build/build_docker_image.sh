4df78d13513de65c2638a477b5a96276afa30708
Gavin <gaprice@lbl.gov>
2024-05-06 17:17:23 -0700

    Remove build cruft
    Obsolete with switch to GHA

--- build/build_docker_image.sh
+++
@@ -1,7 +0,0 @@
-export BRANCH=${TRAVIS_BRANCH:-`git symbolic-ref --short HEAD`}
-export DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-export COMMIT=${TRAVIS_COMMIT:-`git rev-parse --short HEAD`}
-docker build --build-arg BUILD_DATE=$DATE \
-             --build-arg VCS_REF=$COMMIT \
-             --build-arg BRANCH=$BRANCH \
-             -t kbase/blobstore:$COMMIT .
